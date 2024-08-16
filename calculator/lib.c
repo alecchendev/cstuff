50920ca90c44f994f8931d92a1d447a7fcc4144b
Alec Chen <alecchendev@gmail.com>
2024-08-15 22:29:20 -0700

    Extend token utils, move core code to lib.c

---
+++ calculator/lib.c
@@ -0,0 +1,318 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Expression Expression;
+typedef struct Constant Constant;
+typedef struct BinaryExpr BinaryExpr;
+
+struct Constant {
+    double value;
+};
+
+typedef enum {
+    NOOP,
+    ADD,
+    SUB,
+    MUL,
+    DIV
+} BinaryOp;
+
+struct BinaryExpr {
+    BinaryOp operator;
+    Expression *left;
+    Expression *right;
+};
+
+typedef enum {
+    CONSTANT,
+    BINARY_OP,
+    QUIT,
+} ExprType;
+
+typedef union {
+    Constant constant;
+    BinaryExpr binary_expr;
+} ExprData;
+
+struct Expression {
+    ExprType type;
+    ExprData expr;
+};
+
+typedef struct Arena Arena;
+
+struct Arena {
+    size_t size;
+    size_t used;
+    char memory[];
+};
+
+Arena *arena_create(size_t size) {
+    Arena *arena = malloc(sizeof(Arena) + size);
+    arena->size = size;
+    arena->used = 0;
+    // arena->memory already points to the start of the memory block
+    return arena;
+}
+
+void *arena_alloc(Arena *arena, size_t size) {
+    if (arena->used + size > arena->size) {
+        printf("ERROR: Out of memory\n");
+        return NULL; // TODO
+    }
+    void *ptr = arena->memory + arena->used;
+    arena->used += size;
+    return ptr;
+}
+
+void arena_free(Arena *arena) {
+    free(arena);
+}
+
+enum TokenType {
+    NUMBER,
+    BINARY_OPERATOR,
+    WHITESPACE,
+    QUITTOKEN, // TODO: how to name QUIT without conflicts
+    END,
+    INVALID,
+};
+
+typedef union TokenData TokenData;
+union TokenData {
+    double number;
+    BinaryOp binary_operator;
+};
+
+typedef struct Token Token;
+struct Token {
+    enum TokenType type;
+    union TokenData data;
+};
+
+const unsigned int MAX_INPUT = 256;
+const size_t MAX_MEMORY_SIZE = MAX_INPUT * sizeof(Token);
+const char *prompt = ">>> ";
+
+// TODO: turn this into something simpler at comptime
+bool char_in_set(char c, const unsigned char *set) {
+    bool char_set[256] = {0};
+    for (size_t i = 0; set[i] != '\0'; i++) {
+        char_set[set[i]] = true;
+    }
+    return char_set[c];
+}
+
+bool is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+double char_to_digit(char c) {
+    return c - '0';
+}
+
+const Token invalid_token = {INVALID};
+const Token end_token = {END};
+const Token quit_token = {QUITTOKEN};
+const Token whitespace_token = {WHITESPACE};
+
+Token token_new_bin(BinaryOp op) {
+    return (Token){BINARY_OPERATOR, .data = { .binary_operator = op }};
+}
+
+Token token_new_num(double num) {
+    return (Token){NUMBER, .data = { .number = num }};
+}
+
+// TODO: make this more generic where I can simply define
+// basically a table of strings and their corresponding tokens
+Token next_token(const char *input, size_t *pos, size_t length) {
+    const unsigned char end[] = {'\0', '\n'};
+    if (*pos >= length || char_in_set(input[*pos], end)) {
+        if (*pos != length) return invalid_token;
+        return end_token;
+    }
+
+    if (input[*pos] == 'q' || input[*pos] == 'e') {
+        // TODO: handle no spaces after?
+        if (strncmp(input + *pos, "quit", 4) == 0 || strncmp(input + *pos, "exit", 4) == 0) {
+            *pos += 4;
+            return quit_token;
+        }
+    }
+
+    const unsigned char whitespace[] = {' ', '\t'};
+    if (char_in_set(input[*pos], whitespace)) {
+        while (char_in_set(input[*pos], whitespace)) {
+            (*pos)++;
+        }
+        return whitespace_token;
+    }
+
+    const unsigned char operators[] = {'+', '-', '*', '/'};
+    if (char_in_set(input[*pos], operators)) {
+        BinaryOp op;
+        switch (input[*pos]) {
+            case '+':
+                op = ADD;
+                break;
+            case '-':
+                op = SUB;
+                break;
+            case '*':
+                op = MUL;
+                break;
+            case '/':
+                op = DIV;
+                break;
+        }
+        (*pos)++;
+        return token_new_bin(op);
+    }
+
+    if (is_digit(input[*pos])) {
+        double number = 0;
+        while (is_digit(input[*pos])) {
+            number = number * 10 + char_to_digit(input[*pos]);
+            (*pos)++;
+        }
+        if (input[*pos] == '.') {
+            (*pos)++;
+            double decimal = 0.1;
+            while (input[*pos] >= '0' && input[*pos] <= '9') {
+                number += (input[*pos] - '0') * decimal;
+                decimal /= 10;
+                (*pos)++;
+            }
+        }
+        return token_new_num(number);
+    }
+
+    return invalid_token;
+}
+
+typedef struct TokenString TokenString;
+struct TokenString {
+    Token *tokens;
+    size_t length;
+};
+
+TokenString tokenize(const char *input, Arena *arena) {
+    TokenString token_string;
+    token_string.tokens = arena_alloc(arena, sizeof(Token) * MAX_INPUT);
+    token_string.length = 0;
+    bool done = false;
+    size_t pos = 0;
+    size_t input_length = strnlen(input, MAX_INPUT);
+    while (!done) {
+        Token token = next_token(input, &pos, input_length);
+        if (token.type == INVALID || token.type == END || token.type == QUITTOKEN) {
+            done = true;
+        } else if (token.type == WHITESPACE) {
+            continue;
+        }
+        token_string.tokens[token_string.length] = token;
+        token_string.length++;
+    }
+    return token_string;
+}
+
+void token_display(Token token) {
+    switch (token.type) {
+        case END:
+            printf("End of input\n");
+            break;
+        case INVALID:
+            printf("Invalid token\n");
+            break;
+        case QUITTOKEN:
+            printf("Quit token\n");
+            break;
+        case NUMBER:
+            printf("Number: %f\n", token.data.number);
+            break;
+        case BINARY_OPERATOR:
+            printf("Operator: %d\n", token.data.binary_operator);
+            break;
+        default:
+            printf("Unknown token\n");
+            break;
+    }
+}
+
+Expression *parse(char *input, Arena *arena) {
+    Expression *expr = arena_alloc(arena, sizeof(Expression));
+    // Parse binary expression
+    double left_value, right_value;
+    char op;
+    BinaryOp operator = NOOP;
+    if (sscanf(input, "%lf %c %lf", &left_value, &op, &right_value) == 3) {
+        switch (op) {
+            case '+':
+                operator = ADD;
+                break;
+            case '-':
+                operator = SUB;
+                break;
+            case '*':
+                operator = MUL;
+                break;
+            case '/':
+                operator = DIV;
+                break;
+        }
+    }
+    if (operator != NOOP) {
+        BinaryExpr binary_expr;
+        binary_expr.operator = operator;
+
+        binary_expr.left = arena_alloc(arena, sizeof(Expression));
+        binary_expr.left->type = CONSTANT;
+        binary_expr.left->expr.constant.value = left_value;
+
+        binary_expr.right = arena_alloc(arena, sizeof(Expression));
+        binary_expr.right->type = CONSTANT;
+        binary_expr.right->expr.constant.value = right_value;
+
+        expr->type = BINARY_OP;
+        expr->expr.binary_expr = binary_expr;
+        return expr;
+    }
+
+    // Parse single constant
+    double value;
+    if (sscanf(input, "%lf", &value) == 1) {
+        Constant constant = { .value = value };
+        expr->type = CONSTANT;
+        expr->expr.constant = constant;
+        return expr;
+    }
+
+    return NULL;
+}
+
+double evaluate(Expression expr) {
+    if (expr.type == CONSTANT) {
+        return expr.expr.constant.value;
+    }
+    if (expr.type == BINARY_OP) {
+        double left = evaluate(*expr.expr.binary_expr.left);
+        double right = evaluate(*expr.expr.binary_expr.right);
+        printf("%f %f\n", left, right);
+        switch (expr.expr.binary_expr.operator) {
+            case ADD:
+                return left + right;
+            case SUB:
+                return left - right;
+            case MUL:
+                return left * right;
+            case DIV:
+                return left / right;
+            case NOOP:
+                exit(1); // TODO
+        }
+    }
+    exit(1); // TODO
+}
--- calculator/main.c
+++ calculator/main.c
@@ -1,288 +1,48 @@
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "lib.c"
 
-typedef struct Expression Expression;
-typedef struct Constant Constant;
-typedef struct BinaryExpr BinaryExpr;
-
-struct Constant {
-    double value;
-};
-
-typedef enum {
-    NOOP,
-    ADD,
-    SUBTRACT,
-    MULTIPLY,
-    DIVIDE
-} BinaryOp;
-
-struct BinaryExpr {
-    BinaryOp operator;
-    Expression *left;
-    Expression *right;
-};
-
-typedef enum {
-    CONSTANT,
-    BINARY_OP,
-    QUIT,
-} ExprType;
-
-typedef union {
-    Constant constant;
-    BinaryExpr binary_expr;
-} ExprData;
-
-struct Expression {
-    ExprType type;
-    ExprData expr;
-};
-
-typedef struct Arena Arena;
-
-struct Arena {
-    size_t size;
-    size_t used;
-    char memory[];
-};
-
-Arena *arena_create(size_t size) {
-    Arena *arena = malloc(sizeof(Arena) + size);
-    arena->size = size;
-    arena->used = 0;
-    // arena->memory already points to the start of the memory block
-    return arena;
-}
-
-void *arena_alloc(Arena *arena, size_t size) {
-    if (arena->used + size > arena->size) {
-        return NULL; // TODO
-    }
-    void *ptr = arena->memory + arena->used;
-    arena->used += size;
-    return ptr;
-}
-
-void arena_free(Arena *arena) {
-    free(arena);
-}
-
-const unsigned int MAX_INPUT = 256;
-const char *prompt = ">>> ";
-
-enum TokenType {
-    NUMBER,
-    BINARY_OPERATOR,
-    WHITESPACE,
-    QUITTOKEN, // TODO: how to name QUIT without conflicts
-    END,
-    INVALID,
-};
-
-typedef union TokenData TokenData;
-union TokenData {
-    double number;
-    BinaryOp binary_operator;
-};
-
-typedef struct Token Token;
-struct Token {
-    enum TokenType type;
-    union TokenData data;
-};
-
-// TODO: turn this into something simpler at comptime
-bool char_in_set(char c, const unsigned char *set) {
-    bool char_set[256] = {0};
-    for (size_t i = 0; set[i] != '\0'; i++) {
-        char_set[set[i]] = true;
-    }
-    return char_set[c];
-}
-
-bool is_digit(char c) {
-    return c >= '0' && c <= '9';
-}
-
-double char_to_digit(char c) {
-    return c - '0';
-}
-
-Token next_token(char *input, size_t *pos, size_t length) {
-    const Token invalid_token = {INVALID};
-    const Token end_token = {END};
-    const unsigned char end[] = {'\0', '\n'};
-    if (*pos >= length || char_in_set(input[*pos], end)) {
-        if (*pos != length) return invalid_token;
-        return end_token;
-    }
-
-    if (input[*pos] == 'q' || input[*pos] == 'e') {
-        // TODO: handle no spaces after?
-        if (strncmp(input + *pos, "quit", 4) == 0 || strncmp(input + *pos, "exit", 4) == 0) {
-            Token quit_token = {QUITTOKEN};
-            *pos += 4;
-            return quit_token;
-        }
-    }
-
-    const Token whitespace_token = {WHITESPACE};
-    const unsigned char whitespace[] = {' ', '\t'};
-    if (char_in_set(input[*pos], whitespace)) {
-        while (char_in_set(input[*pos], whitespace)) {
-            (*pos)++;
-        }
-        return whitespace_token;
-    }
-
-    const unsigned char operators[] = {'+', '-', '*', '/'};
-    if (char_in_set(input[*pos], operators)) {
-        TokenData data;
-        switch (input[*pos]) {
-            case '+':
-                data.binary_operator = ADD;
-                break;
-            case '-':
-                data.binary_operator = SUBTRACT;
-                break;
-            case '*':
-                data.binary_operator = MULTIPLY;
-                break;
-            case '/':
-                data.binary_operator = DIVIDE;
-                break;
-        }
-        Token bin_token = {BINARY_OPERATOR, data};
-        (*pos)++;
-        return bin_token;
-    }
-
-    if (is_digit(input[*pos])) {
-        double number = 0;
-        while (is_digit(input[*pos])) {
-            number = number * 10 + char_to_digit(input[*pos]);
-            (*pos)++;
-        }
-        if (input[*pos] == '.') {
-            (*pos)++;
-            double decimal = 0.1;
-            while (input[*pos] >= '0' && input[*pos] <= '9') {
-                number += (input[*pos] - '0') * decimal;
-                decimal /= 10;
-                (*pos)++;
-            }
-        }
-        TokenData data = { .number = number };
-        Token num_token = {NUMBER, data};
-        return num_token;
-    }
-
-    return invalid_token;
-}
-
-Expression *parse(char *input, Arena *arena) {
-    Expression *expr = arena_alloc(arena, sizeof(Expression));
-    if (strnlen(input, MAX_INPUT) == 4 && (strncmp(input, "quit", 4) == 0 || strncmp(input, "exit", 4) == 0)) {
-        expr->type = QUIT;
-        return expr;
-    }
-    // Parse binary expression
-    double left_value, right_value;
-    char op;
-    BinaryOp operator = NOOP;
-    if (sscanf(input, "%lf %c %lf", &left_value, &op, &right_value) == 3) {
-        switch (op) {
-            case '+':
-                operator = ADD;
-                break;
-            case '-':
-                operator = SUBTRACT;
-                break;
-            case '*':
-                operator = MULTIPLY;
-                break;
-            case '/':
-                operator = DIVIDE;
-                break;
-        }
-    }
-    if (operator != NOOP) {
-        BinaryExpr binary_expr;
-        binary_expr.operator = operator;
-
-        binary_expr.left = arena_alloc(arena, sizeof(Expression));
-        binary_expr.left->type = CONSTANT;
-        binary_expr.left->expr.constant.value = left_value;
-
-        binary_expr.right = arena_alloc(arena, sizeof(Expression));
-        binary_expr.right->type = CONSTANT;
-        binary_expr.right->expr.constant.value = right_value;
-
-        expr->type = BINARY_OP;
-        expr->expr.binary_expr = binary_expr;
-        return expr;
-    }
-
-    // Parse single constant
-    double value;
-    if (sscanf(input, "%lf", &value) == 1) {
-        Constant constant = { .value = value };
-        expr->type = CONSTANT;
-        expr->expr.constant = constant;
-        return expr;
-    }
-
-    return NULL;
-}
-
-double evaluate(Expression expr) {
-    if (expr.type == CONSTANT) {
-        return expr.expr.constant.value;
-    }
-    if (expr.type == BINARY_OP) {
-        double left = evaluate(*expr.expr.binary_expr.left);
-        double right = evaluate(*expr.expr.binary_expr.right);
-        printf("%f %f\n", left, right);
-        switch (expr.expr.binary_expr.operator) {
-            case ADD:
-                return left + right;
-            case SUBTRACT:
-                return left - right;
-            case MULTIPLY:
-                return left * right;
-            case DIVIDE:
-                return left / right;
-            case NOOP:
-                exit(1); // TODO
-        }
-    }
-    exit(1); // TODO
-}
 
 int main() {
     char input[MAX_INPUT];
     FILE *input_fd = stdin;
-    while (1) {
+    bool done = false;
+    while (!done) {
         printf("%s", prompt);
         if (fgets(input, sizeof(input), input_fd) == NULL) {
             break;
         }
         input[strcspn(input, "\n")] = '\0';
-        Arena *arena = arena_create(1024);
-        Expression *expr = parse(input, arena);
-        if (expr == NULL) {
-            printf("Invalid expression\n");
-            continue;
+        Arena *arena = arena_create(MAX_MEMORY_SIZE);
+        TokenString tokens = tokenize(input, arena);
+
+        for (size_t i = 0; i < tokens.length; i++) {
+            Token token = tokens.tokens[i];
+            if (token.type == END) {
+                printf("End of input\n");
+            } else if (token.type == INVALID) {
+                printf("Invalid token\n");
+            } else if (token.type == QUITTOKEN) {
+                printf("Quit token\n");
+                done = true;
+            } else if (token.type == NUMBER) {
+                printf("Number: %f\n", token.data.number);
+            } else if (token.type == BINARY_OPERATOR) {
+                printf("Operator: %d\n", token.data.binary_operator);
+            } else {
+                printf("Unknown token\n");
+            }
         }
-        if (expr->type == QUIT) {
-            break;
-        }
-        double result = evaluate(*expr);
-        printf("%f\n", result);
+
+        /*Expression *expr = parse(input, arena);*/
+        /*if (expr == NULL) {*/
+        /*    printf("Invalid expression\n");*/
+        /*    continue;*/
+        /*}*/
+        /*if (expr->type == QUIT) {*/
+        /*    break;*/
+        /*}*/
+        /*double result = evaluate(*expr);*/
+        /*printf("%f\n", result);*/
         arena_free(arena);
     }
     return 0;
